6f8f8ff0afeee2e76079efd123ddfbad34f808ae
leyla-h <13678@holbertonstudents.com>
2026-04-20 22:30:45 +0400

    Create 17-integrate.py

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module to calculate integral"""
+
+
+def poly_integral(poly, C=0):
+    """Calculates integral"""
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    if not isinstance(C, (int, float)):
+        return None
+
+    res = [C]
+    for i in range(len(poly)):
+        if not isinstance(poly[i], (int, float)):
+            return None
+        
+        val = poly[i] / (i + 1)
+        
+        if val % 1 == 0:
+            res.append(int(val))
+        else:
+            res.append(val)
+
+    while len(res) > 1 and res[-1] == 0:
+        res.pop()
+
+    return res
