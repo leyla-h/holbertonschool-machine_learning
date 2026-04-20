3cd93fd76cd4327c4d9b4d81ce89a4238683546e
leyla-h <13678@holbertonstudents.com>
2026-04-20 22:31:54 +0400

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -8,20 +8,15 @@ def poly_integral(poly, C=0):
         return None
     if not isinstance(C, (int, float)):
         return None
-
     res = [C]
     for i in range(len(poly)):
         if not isinstance(poly[i], (int, float)):
             return None
-        
         val = poly[i] / (i + 1)
-        
         if val % 1 == 0:
             res.append(int(val))
         else:
             res.append(val)
-
     while len(res) > 1 and res[-1] == 0:
         res.pop()
-
     return res
