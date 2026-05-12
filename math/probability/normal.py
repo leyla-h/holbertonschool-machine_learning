2d6393ceffc7f7215fb327a914bee98ada264791
leyla-h <13678@holbertonstudents.com>
2026-05-12 21:39:57 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -39,3 +39,15 @@ class Normal:
         Calculates the x-value of a given z-score.
         """
         return self.mean + (z * self.stddev)
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given x-value.
+        """
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        exponent = -0.5 * (self.z_score(x) ** 2)
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+
+        return coefficient * (e ** exponent)
