7397a85d891dafdbc111920fba5a8f8b88eba3c2
leyla-h <13678@holbertonstudents.com>
2026-05-12 21:36:11 +0400

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -13,22 +13,39 @@ class Exponential:
     def __init__(self, data=None, lambtha=1.):
         """
         Initializes the Exponential distribution.
-
-        Args:
-            data (list): List of data to estimate the distribution.
-            lambtha (float): Expected number of occurrences.
         """
         if data is None:
-            # Check lambtha if data is not provided
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            # Validate data if provided
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
-            # Calculate lambtha (inverse of the mean of data)
             self.lambtha = float(1 / (sum(data) / len(data)))
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given time period.
+        """
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+        pdf_val = self.lambtha * (e ** (-self.lambtha * x))
+
+        return pdf_val
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period.
+        """
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+        cdf_val = 1 - (e ** (-self.lambtha * x))
+
+        return cdf_val
