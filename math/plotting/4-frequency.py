b098dc2253c7e8f76b689689d29d740d590682b2
leyla-h <13678@holbertonstudents.com>
2026-05-25 01:23:52 +0400

    Create 4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module to plot a histogram of student grades.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Plots a histogram tracking the distribution of student scores.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+
+    plt.show()
