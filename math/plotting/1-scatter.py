e54108ccb6dd14474635dfed83f198dbe1ad61f1
leyla-h <13678@holbertonstudents.com>
2026-05-25 01:16:50 +0400

    Update 1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -17,7 +17,7 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x, y, color='m', marker='.')
+    plt.scatter(x, y, c='m')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.title("Men's Height vs Weight")
