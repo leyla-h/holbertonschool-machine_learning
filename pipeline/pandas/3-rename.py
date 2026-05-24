147e95811da061028d394d9af291ae288fae3dad
leyla-h <13678@holbertonstudents.com>
2026-05-25 00:45:02 +0400

    Update 3-rename.py

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 """
-Module to rename columns, convert timestamps, and filter columns in a DataFrame.
+Module to rename columns, convert timestamps, and filter columns.
 """
 import pandas as pd
 
 
 def rename(df):
     """
-    Renames the Timestamp column to Datetime, converts its values to
-    datetime objects, and filters the DataFrame to show only Datetime and Close.
+    Renames Timestamp to Datetime, converts values to datetime objects,
+    and filters the DataFrame to show only Datetime and Close.
 
     Args:
         df: pd.DataFrame containing a column named Timestamp
 
     Returns:
-        The modified pd.DataFrame containing only Datetime and Close columns
+        The modified pd.DataFrame containing only Datetime and Close
     """
     df = df.rename(columns={'Timestamp': 'Datetime'})
 
