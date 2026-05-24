52d55fc836b0866a35e58cc0f8c2adad79c07c7c
leyla-h <13678@holbertonstudents.com>
2026-05-25 01:20:10 +0400

    Create 3-two.py

---
+++ math/plotting/3-two.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module to plot exponential decay of two distinct radioactive elements.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """
+    Plots two decay curves with individual styles, axis bounds, and a legend.
+    """
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y1, '#F14223', linestyle='dashed', label='C-14')
+    plt.plot(x, y2, '#33811B', linestyle='solid', label='Ra-226')
+
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+
+    plt.legend(loc='upper right')
+    plt.show()
