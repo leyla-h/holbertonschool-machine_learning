c8704b541d4c6bcb1a3bd609289094016be5a7b9
leyla-h <13678@holbertonstudents.com>
2026-05-25 01:11:51 +0400

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module to plot a mathematical line graph using matplotlib.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """
+    Plots a cubic function as a solid red line with defined x-axis limits.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
