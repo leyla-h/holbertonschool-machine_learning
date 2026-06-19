267fdb935b002f2201bc58433530cb3eaa77a71b
leyla-h <13678@holbertonstudents.com>
2026-06-20 00:33:54 +0400

    Update 0-build_decision_tree.py

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""Module to build a decision tree"""
 import numpy as np
 
 
 class Node:
+    """Class representing a node in a decision tree"""
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
+        """Initializes a Node"""
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -15,25 +18,30 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        left = self.left_child.max_depth_below()
-        right = self.right_child.max_depth_below()
-        return max(left, right)
+        """Calculates the maximum depth below this node"""
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
 
 
 class Leaf(Node):
+    """Class representing a leaf in a decision tree"""
     def __init__(self, value, depth=None):
+        """Initializes a Leaf"""
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def max_depth_below(self):
+        """Returns the depth of the leaf"""
         return self.depth
 
 
-class Decision_Tree():
+class Decision_Tree:
+    """Class representing a decision tree"""
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
+        """Initializes a Decision_Tree"""
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -47,4 +55,5 @@ class Decision_Tree():
         self.predict = None
 
     def depth(self):
+        """Returns the depth of the decision tree"""
         return self.root.max_depth_below()
