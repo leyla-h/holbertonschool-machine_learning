ac54e901ce53bea3294e7364d9ce41869254bffb
leyla-h <13678@holbertonstudents.com>
2026-06-29 10:21:29 +0400

    Create 8-neural_network.py

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+This module defines the NeuralNetwork class for binary classification.
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Defines a neural network with one hidden layer performing
+    binary classification.
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        Initializes the neural network.
+
+        Args:
+            nx (int): Number of input features.
+            nodes (int): Number of nodes in the hidden layer.
+
+        Raises:
+            TypeError: If nx or nodes are not integers.
+            ValueError: If nx or nodes are not positive integers.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
