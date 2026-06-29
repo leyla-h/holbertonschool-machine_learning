8fd1eb2863b8e8f3d8457463893fb4b1e876d3ef
leyla-h <13678@holbertonstudents.com>
2026-06-29 10:13:15 +0400

    Create 3-neuron.py

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+This module defines the Neuron class with a cost function.
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification.
+    """
+
+    def __init__(self, nx):
+        """
+        Initializes the neuron.
+
+        Args:
+            nx (int): The number of input features to the neuron.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weight vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron.
+        """
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): Correct labels with shape (1, m).
+            A (numpy.ndarray): Activated output with shape (1, m).
+
+        Returns:
+            float: The cost.
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
