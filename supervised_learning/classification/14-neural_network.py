f7648e49ee42273c281e10ac9bb6c781d6036e5b
leyla-h <13678@holbertonstudents.com>
2026-06-29 10:28:19 +0400

    Create 14-neural_network.py

---
+++ supervised_learning/classification/14-neural_network.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""This module defines the NeuralNetwork class."""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Defines a neural network with one hidden layer."""
+
+    def __init__(self, nx, nodes):
+        """Initializes the neural network."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for W1."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2."""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates forward propagation."""
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates cost using logistic regression."""
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) *
+                                 np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates neural network."""
+        _, A = self.forward_prop(X)
+        return (np.where(A >= 0.5, 1, 0), self.cost(Y, A))
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Calculates one pass of gradient descent."""
+        m = Y.shape[1]
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.matmul(dZ2, A1.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = (1 / m) * np.matmul(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Trains the neural network."""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+
+        for _ in range(iterations):
+            A1, A2 = self.forward_prop(X)
+            self.gradient_descent(X, Y, A1, A2, alpha)
+
+        return self.evaluate(X, Y)
