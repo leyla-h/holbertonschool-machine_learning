d1e6341b99296db24ec6d60e9eb897f2cc805d5a
leyla-h <13678@holbertonstudents.com>
2026-07-07 22:02:03 +0400

    Create 5-momentum.py

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module to update a variable using gradient descent with momentum.
+"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using gradient descent with momentum.
+
+    Args:
+        alpha: The learning rate.
+        beta1: The momentum weight.
+        var: The variable to be updated.
+        grad: The gradient of var.
+        v: The previous first moment of var.
+
+    Returns:
+        The updated variable and the new moment, respectively.
+    """
+    v_new = beta1 * v + (1 - beta1) * grad
+    
+    var_new = var - alpha * v_new
+    
+    return var_new, v_new
