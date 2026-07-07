c900758a7ebee1ca99fd7e86982a884c796c3614
leyla-h <13678@holbertonstudents.com>
2026-07-07 21:48:19 +0400

    Create 12-test.py

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module to test a Keras model.
+"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Tests a neural network.
+
+    Args:
+        network: The network model to test.
+        data: The input data to test the model with.
+        labels: The correct one-hot labels of data.
+        verbose: A boolean that determines if output should be
+            printed during the testing process.
+
+    Returns:
+        The loss and accuracy of the model with the testing data.
+    """
+    results = network.evaluate(x=data, y=labels, verbose=verbose)
+    return results
