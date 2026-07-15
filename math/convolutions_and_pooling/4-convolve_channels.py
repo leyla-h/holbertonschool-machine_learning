038a14ae1edcd6bdf58fb2418fa657cfe027a5c1
leyla-h <13678@holbertonstudents.com>
2026-07-15 21:13:40 +0400

    Create 4-convolve_channels.py

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Performs a convolution on images with channels.
+"""
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """
+    images is a numpy.ndarray with shape (m, h, w, c)
+    kernel is a numpy.ndarray with shape (kh, kw, c)
+    padding is either a tuple of (ph, pw), 'same', or 'valid'
+    stride is a tuple of (sh, sw)
+    Returns: a numpy.ndarray containing the convolved images
+    """
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = int(((h - 1) * sh + kh - h) / 2) + 1
+        pw = int(((w - 1) * sw + kw - w) / 2) + 1
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                           'constant')
+
+    oh = int((h + (2 * ph) - kh) / sh) + 1
+    ow = int((w + (2 * pw) - kw) / sw) + 1
+
+    output = np.zeros((m, oh, ow))
+
+    for i in range(oh):
+        for j in range(ow):
+            output[:, i, j] = np.sum(
+                padded_images[:, i * sh:i * sh + kh, j * sw:j * sw + kw] *
+                kernel, axis=(1, 2, 3)
+            )
+
+    return output
