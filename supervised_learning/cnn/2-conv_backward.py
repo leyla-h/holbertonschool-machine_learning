29aa5b320176a1ec68f96133e2b7d2ddb4a6ddb7
leyla-h <13678@holbertonstudents.com>
2026-07-21 19:35:22 +0400

    Create 2-conv_backward.py

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+"""
+Convolutional Backward Propagation
+"""
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """
+    Performs back propagation over a convolutional layer of a neural network.
+    """
+    m, h_new, w_new, c_new = dZ.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
+        pw = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
+    elif padding == 'valid':
+        ph = 0
+        pw = 0
+    else:
+        return
+
+    # Pad A_prev and dA_prev
+    A_prev_pad = np.pad(
+        A_prev,
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode='constant',
+        constant_values=0
+    )
+    dA_prev_pad = np.pad(
+        np.zeros_like(A_prev),
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode='constant',
+        constant_values=0
+    )
+
+    dW = np.zeros_like(W)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    a_slice = a_prev_pad[vert_start:vert_end,
+                                         horiz_start:horiz_end, :]
+
+                    da_prev_pad[vert_start:vert_end,
+                                horiz_start:horiz_end, :] += \
+                        W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+
+        if padding == 'same':
+            dA_prev_pad[i, :, :, :] = da_prev_pad[ph:-ph, pw:-pw, :] \
+                if ph > 0 and pw > 0 else (
+                    da_prev_pad[ph:, pw:] if ph > 0 else (
+                        da_prev_pad[:, pw:-pw] if pw > 0 else da_prev_pad
+                    )
+                )
+        else:
+            dA_prev_pad[i, :, :, :] = da_prev_pad
+
+    if padding == 'same':
+        if ph > 0 and pw > 0:
+            dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+        elif ph > 0:
+            dA_prev = dA_prev_pad[:, ph:-ph, :, :]
+        elif pw > 0:
+            dA_prev = dA_prev_pad[:, :, pw:-pw, :]
+        else:
+            dA_prev = dA_prev_pad
+    else:
+        dA_prev = dA_prev_pad
+
+    return dA_prev, dW, db
