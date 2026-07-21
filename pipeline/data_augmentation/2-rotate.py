320e60caa4b6e7d4821b55fed6420ecfbd752e50
leyla-h <13678@holbertonstudents.com>
2026-07-21 19:53:15 +0400

    Create 2-rotate.py

---
+++ pipeline/data_augmentation/2-rotate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Rotate Image
+"""
+import tensorflow as tf
+
+
+def rotate_image(image):
+    """
+    Rotates an image by 90 degrees counter-clockwise.
+    """
+    return tf.image.rot90(image, k=1)
