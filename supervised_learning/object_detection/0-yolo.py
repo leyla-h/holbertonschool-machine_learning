cf538fdce34f6cfba17a93b4d20456c72f1d86db
leyla-h <13678@holbertonstudents.com>
2026-07-29 00:32:11 +0400

    Update 0-yolo.py

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -17,10 +17,8 @@ class Yolo:
         Class constructor for Yolo.
         """
         self.model = K.models.load_model(model_path)
-        
         with open(classes_path, 'r') as f:
             self.class_names = [line.strip() for line in f.readlines()]
-            
         self.class_t = float(class_t)
         self.nms_t = float(nms_t)
         self.anchors = anchors
