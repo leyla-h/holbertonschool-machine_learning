9e7577d3977187e7823763b41e7514359c93743c
leyla-h <13678@holbertonstudents.com>
2026-07-29 14:32:03 +0400

    Update 5-yolo.py

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -230,15 +230,13 @@ class Yolo:
         for img in images:
             image_shapes.append(img.shape[:2])
 
-            img_float = img.astype(np.float32) / 255.0
-
             resized = cv2.resize(
-                img_float,
+                img,
                 (input_w, input_h),
                 interpolation=cv2.INTER_CUBIC
             )
-
-            pimages.append(resized)
+            rescaled = resized.astype(np.float32) / 255.0
+            pimages.append(rescaled)
 
         pimages = np.array(pimages, dtype=np.float32)
         image_shapes = np.array(image_shapes)
