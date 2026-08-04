56f3489f29bdf59ce1a920698523196283391942
leyla-h <13678@holbertonstudents.com>
2026-08-04 16:05:55 +0400

    Create 2-variance.py

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Calculates the total intra-cluster variance for a data set"""
+import numpy as np
+
+
+def variance(X, C):
+    """
+    Calculates the total intra-cluster variance for a data set
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    C is a numpy.ndarray of shape (k, d) containing the centroid
+        means for each cluster
+
+    Returns: var, or None on failure
+        var is the total variance
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+    min_distances = np.min(distances, axis=1)
+    var = np.sum(np.square(min_distances))
+
+    return var
