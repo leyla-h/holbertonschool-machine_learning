cced8dcda7a9d0833aae3e170ea5d2a2755a5251
leyla-h <13678@holbertonstudents.com>
2026-08-04 16:07:24 +0400

    Create 3-optimum.py

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Tests for the optimum number of clusters by variance"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Tests for the optimum number of clusters by variance
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    kmin is a positive integer containing the minimum number of
+        clusters to check for (inclusive)
+    kmax is a positive integer containing the maximum number of
+        clusters to check for (inclusive)
+    iterations is a positive integer containing the maximum
+        number of iterations for K-means
+
+    This function should analyze at least 2 different cluster
+        sizes
+
+    Returns: results, d_vars, or None, None on failure
+        results is a list containing the outputs of K-means for
+            each cluster size
+        d_vars is a list containing the difference in variance
+            from the smallest cluster size for each cluster size
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0]
+    if not isinstance(kmax, int) or kmax <= 0:
+        return None, None
+    if kmin >= kmax:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+
+    results = []
+    variances = []
+
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        if C is None or clss is None:
+            return None, None
+        results.append((C, clss))
+        variances.append(variance(X, C))
+
+    d_vars = [variances[0] - v for v in variances]
+
+    return results, d_vars
