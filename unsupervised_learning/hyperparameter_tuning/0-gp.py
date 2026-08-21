84c1e1a3383efab0da9da21b8e45920ea14fe6b7
leyla-h <13678@holbertonstudents.com>
2026-08-21 20:56:07 +0400

    Create 0-gp.py

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module for a noiseless 1D Gaussian Process."""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process."""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Class constructor."""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two matrices
+        using the Radial Basis Function (RBF).
+        """
+        # RBF kernel formula: K(X1, X2) = sigma_f^2 * exp(-0.5 / l^2 * ||X1 - X2||^2)
+        # Using broadcasting: (m, 1) - (1, n) -> (m, n)
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
