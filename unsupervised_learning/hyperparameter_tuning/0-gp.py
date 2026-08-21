d752a499b33fd5f7215a05ce308307de27ad35a0
leyla-h <13678@holbertonstudents.com>
2026-08-21 20:59:22 +0400

    Update 0-gp.py

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -21,8 +21,8 @@ class GaussianProcess:
         using the Radial Basis Function (RBF).
         """
         sqdist = (
-            np.sum(X1**2, 1).reshape(-1, 1)
-            + np.sum(X2**2, 1)
-            - 2 * np.dot(X1, X2.T)
+            np.sum(X1 ** 2, 1).reshape(-1, 1) +
+            np.sum(X2 ** 2, 1) -
+            2 * np.dot(X1, X2.T)
         )
         return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
