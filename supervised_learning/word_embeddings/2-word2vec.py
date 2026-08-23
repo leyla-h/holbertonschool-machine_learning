6963e4c178d95bc7712bf1d6546bc6019b3f54ba
leyla-h <13678@holbertonstudents.com>
2026-08-23 15:10:59 +0400

    Update 2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Defines a function that builds and trains a gensim word2vec model"""
-from gensim.models import Word2Vec
+import gensim
 
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
+def word2vec_model(
+        sentences, vector_size=100, min_count=5, window=5,
+        negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     Creates, builds, and trains a gensim word2vec model
 
@@ -27,18 +28,20 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     """
     sg = 0 if cbow else 1
 
-    model = Word2Vec(sentences=sentences,
-                      vector_size=vector_size,
-                      min_count=min_count,
-                      window=window,
-                      negative=negative,
-                      sg=sg,
-                      seed=seed,
-                      workers=workers,
-                      epochs=epochs)
+    model = gensim.models.Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        seed=seed,
+        workers=workers,
+        epochs=epochs)
 
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=model.epochs)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs)
 
     return model
