4eead81f38e1d33e418c3602602fcec674f3537d
leyla-h <13678@holbertonstudents.com>
2026-08-28 20:56:07 +0400

    Update 1-gp.py

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-"""Module for a noiseless 1D Gaussian Process with prediction capabilities."""
-
+"""Gaussian Process Prediction"""
 import numpy as np
 
 
 class GaussianProcess:
-    """Represents a noiseless 1D Gaussian process."""
+    """represents a noiseless 1D Gaussian process"""
 
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """Class constructor."""
+        """
+        X_init is a numpy.ndarray of shape (t, 1) representing the inputs
+            already sampled with the black-box function
+        Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
+            of the black-box function for each input in X_init
+        t is the number of initial samples
+        l is the length parameter for the kernel
+        sigma_f is the standard deviation given to the output of the
+            black-box function
+        """
         self.X = X_init
         self.Y = Y_init
         self.l = l
@@ -17,32 +25,41 @@ class GaussianProcess:
 
     def kernel(self, X1, X2):
         """
-        Calculates the covariance kernel matrix between two matrices
-        using the Radial Basis Function (RBF).
+        calculates the covariance kernel matrix between two matrices
+        X1 is a numpy.ndarray of shape (m, 1)
+        X2 is a numpy.ndarray of shape (n, 1)
+        the kernel uses the Radial Basis Function (RBF)
+        Returns: the covariance kernel matrix as a numpy.ndarray
+            of shape (m, n)
         """
-        sqdist = (
-            np.sum(X1 ** 2, 1).reshape(-1, 1)
-            + np.sum(X2 ** 2, 1)
-            - 2 * np.dot(X1, X2.T)
-        )
+        a = np.sum(X1 ** 2, axis=1).reshape(-1, 1)
+        b = np.sum(X2 ** 2, axis=1)
+        c = 2 * np.dot(X1, X2.T)
+        sqdist = a + b - c
         return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
 
     def predict(self, X_s):
         """
-        Predicts the mean and standard deviation (variance) of points
-        in a Gaussian process.
+        predicts the mean and standard deviation of points in a
+        Gaussian process
+        X_s is a numpy.ndarray of shape (s, 1) containing all of the
+            points whose mean and standard deviation should be calculated
+            s is the number of sample points
+        Returns: mu, sigma
+            mu is a numpy.ndarray of shape (s,) containing the mean for
+                each point in X_s, respectively
+            sigma is a numpy.ndarray of shape (s,) containing the variance
+                for each point in X_s, respectively
         """
         K = self.K
         K_s = self.kernel(self.X, X_s)
         K_ss = self.kernel(X_s, X_s)
-
-        # Inverse of K
         K_inv = np.linalg.inv(K)
 
-        # Mean: mu = K_s.T * K_inv * Y
-        mu = np.dot(K_s.T, np.dot(K_inv, self.Y)).reshape(-1)
+        mu_s = K_s.T.dot(K_inv).dot(self.Y)
+        mu = mu_s.reshape(-1)
 
-        # Variance/Sigma: sigma = K_ss - K_s.T * K_inv * K_s
-        sigma = np.diagonal(K_ss - np.dot(K_s.T, np.dot(K_inv, K_s)))
+        cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
+        sigma = np.diag(cov_s)
 
         return mu, sigma
