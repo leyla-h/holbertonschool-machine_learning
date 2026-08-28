4a23ee621ac815898142d6b326b858f9b649132c
leyla-h <13678@holbertonstudents.com>
2026-08-28 20:35:52 +0400

    Update 0-gp.py

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Initialize a noiseless 1D Gaussian process"""
+"""Initialize Gaussian Process"""
 import numpy as np
 
 
@@ -8,15 +8,13 @@ class GaussianProcess:
 
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
-        Class constructor
-
-        X_init: numpy.ndarray of shape (t, 1) representing the inputs
+        X_init is a numpy.ndarray of shape (t, 1) representing the inputs
             already sampled with the black-box function
-        Y_init: numpy.ndarray of shape (t, 1) representing the outputs
+        Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
             of the black-box function for each input in X_init
-        t: the number of initial samples
-        l: the length parameter for the kernel
-        sigma_f: the standard deviation given to the output of the
+        t is the number of initial samples
+        l is the length parameter for the kernel
+        sigma_f is the standard deviation given to the output of the
             black-box function
         """
         self.X = X_init
@@ -27,16 +25,14 @@ class GaussianProcess:
 
     def kernel(self, X1, X2):
         """
-        Calculates the covariance kernel matrix between two matrices
-        using the Radial Basis Function (RBF)
-
-        X1: numpy.ndarray of shape (m, 1)
-        X2: numpy.ndarray of shape (n, 1)
-
-        Returns: the covariance kernel matrix as a numpy.ndarray
-            of shape (m, n)
+        calculates the covariance kernel matrix between two matrices
+        X1 is a numpy.ndarray of shape (m, 1)
+        X2 is a numpy.ndarray of shape (n, 1)
+        the kernel uses the Radial Basis Function (RBF)
+        Returns: the covariance kernel matrix as a numpy.ndarray of
+            shape (m, n)
         """
-        sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1)
-                  + np.sum(X2 ** 2, 1)
-                  - 2 * np.dot(X1, X2.T))
+        sqdist = (np.sum(X1 ** 2, axis=1).reshape(-1, 1) +
+                  np.sum(X2 ** 2, axis=1) -
+                  2 * np.dot(X1, X2.T))
         return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
