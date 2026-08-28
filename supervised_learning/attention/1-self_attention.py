070d49b7b09560217eeb53558994b807dd399599
leyla-h <13678@holbertonstudents.com>
2026-08-28 19:56:26 +0400

    Create 1-self_attention.py

---
+++ supervised_learning/attention/1-self_attention.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Self Attention module for machine translation"""
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """Calculates the attention for machine translation"""
+
+    def __init__(self, units):
+        """
+        Class constructor
+
+        units is an integer representing the number of hidden units in
+            the alignment model
+        """
+        super(SelfAttention, self).__init__()
+        self.W = tf.keras.layers.Dense(units)
+        self.U = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, s_prev, hidden_states):
+        """
+        s_prev is a tensor of shape (batch, units) containing the
+            previous decoder hidden state
+        hidden_states is a tensor of shape (batch, input_seq_len, units)
+            containing the outputs of the encoder
+
+        Returns: context, weights
+            context is a tensor of shape (batch, units) that contains
+                the context vector for the decoder
+            weights is a tensor of shape (batch, input_seq_len, 1) that
+                contains the attention weights
+        """
+        s_prev_expanded = tf.expand_dims(s_prev, 1)
+        score = self.V(tf.nn.tanh(self.W(s_prev_expanded) +
+                                   self.U(hidden_states)))
+        weights = tf.nn.softmax(score, axis=1)
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
+        return context, weights
