4081bd99fb895a3f9ae1e3ace3e3d82dbd4fa47e
leyla-h <13678@holbertonstudents.com>
2026-08-29 00:02:42 +0400

    Update 1-td_lambtha.py

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
-                alpha=0.1, gamma=0.99):
+        alpha=0.1, gamma=0.99):
     """
     Performs the TD(λ) algorithm
 
