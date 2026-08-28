c1a11537e9e4ffb87184692d071465d19caea15e
leyla-h <13678@holbertonstudents.com>
2026-08-28 20:58:52 +0400

    Update 4-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -1,48 +1,64 @@
 #!/usr/bin/env python3
-"""Module for Bayesian Optimization acquisition."""
-
+"""Bayesian Optimization - Acquisition"""
 import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """Represents a Bayesian optimization on a noiseless 1D Gaussian process."""
+    """performs Bayesian optimization on a noiseless 1D Gaussian process"""
 
-    def __init__(
-        self,
-        f,
-        X_init,
-        Y_init,
-        bounds,
-        ac_samples,
-        l=1,
-        sigma_f=1,
-        xsi=0.01,
-        minimize=True
-    ):
-        """Class constructor."""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """
+        f is the black-box function to be optimized
+        X_init is a numpy.ndarray of shape (t, 1) representing the inputs
+            already sampled with the black-box function
+        Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
+            of the black-box function for each input in X_init
+        t is the number of initial samples
+        bounds is a tuple of (min, max) representing the bounds of the
+            space in which to look for the optimal point
+        ac_samples is the number of samples that should be analyzed
+            during acquisition
+        l is the length parameter for the kernel
+        sigma_f is the standard deviation given to the output of the
+            black-box function
+        xsi is the exploration-exploitation factor for acquisition
+        minimize is a bool determining whether optimization should be
+            performed for minimization (True) or maximization (False)
+        """
         self.f = f
-        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        min_b, max_b = bounds
+        self.X_s = np.linspace(min_b, max_b, ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
 
     def acquisition(self):
-        """Calculates the next best sample location using Expected Improvement."""
+        """
+        calculates the next best sample location using the Expected
+        Improvement acquisition function
+        Returns: X_next, EI
+            X_next is a numpy.ndarray of shape (1,) representing the next
+                best sample point
+            EI is a numpy.ndarray of shape (ac_samples,) containing the
+                expected improvement of each potential sample
+        """
         mu, sigma = self.gp.predict(self.X_s)
 
-        if self.minimize:
-            Y_opt = np.min(self.gp.Y)
-            improvement = Y_opt - mu - self.xsi
+        if self.minimize is True:
+            f_opt = np.min(self.gp.Y)
+            imp = f_opt - mu - self.xsi
         else:
-            Y_opt = np.max(self.gp.Y)
-            improvement = mu - Y_opt - self.xsi
+            f_opt = np.max(self.gp.Y)
+            imp = mu - f_opt - self.xsi
 
         with np.errstate(divide='warn'):
-            Z = improvement / sigma
-            EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+            Z = imp / sigma
+            EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
             EI[sigma == 0.0] = 0.0
 
         X_next = self.X_s[np.argmax(EI)]
+
         return X_next, EI
