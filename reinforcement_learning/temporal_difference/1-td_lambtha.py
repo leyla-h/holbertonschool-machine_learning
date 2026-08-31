9b10529f7e4d5e07839e71f983e38395c6a7d43e
leyla-h <13678@holbertonstudents.com>
2026-08-31 15:25:06 +0400

    Update 1-td_lambtha.py

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -4,10 +4,9 @@ import numpy as np
 
 
 def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
-        alpha=0.1, gamma=0.99):
+               alpha=0.1, gamma=0.99):
     """
     Performs the TD(λ) algorithm
-
     Args:
         env: environment instance
         V: numpy.ndarray of shape (s,) containing the value estimate
@@ -18,26 +17,21 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
         max_steps: maximum number of steps per episode
         alpha: learning rate
         gamma: discount rate
-
     Returns:
         V, the updated value estimate
     """
     for ep in range(episodes):
         state, _ = env.reset()
         eligibility = np.zeros(V.shape[0])
-
         for step in range(max_steps):
             action = policy(state)
-            new_state, reward, terminated, truncated, info = env.step(action)
-
+            new_state, reward, terminated, truncated, info = env.step(
+                action)
             eligibility *= gamma * lambtha
             eligibility[state] += 1.0
-
             td_error = reward + gamma * V[new_state] - V[state]
             V = V + alpha * td_error * eligibility
-
             state = new_state
             if terminated or truncated:
                 break
-
     return V
