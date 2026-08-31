3c10b4b09677ddacf20a62967ff90e63745e979a
leyla-h <13678@holbertonstudents.com>
2026-08-31 15:04:23 +0400

    Update 10-transformer_decoder.py

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Transformer Decoder module"""
+"""Transformer Decoder"""
 import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
@@ -10,42 +10,46 @@ class Decoder(tf.keras.layers.Layer):
 
     def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len,
                  drop_rate=0.1):
-        """
-        Class constructor
+        """Class constructor
 
-        N - the number of blocks in the encoder
-        dm - the dimensionality of the model
-        h - the number of heads
-        hidden - the number of hidden units in the fully connected layer
-        target_vocab - the size of the target vocabulary
-        max_seq_len - the maximum sequence length possible
-        drop_rate - the dropout rate
+        Args:
+            N: the number of blocks in the encoder
+            dm: the dimensionality of the model
+            h: the number of heads
+            hidden: the number of hidden units in the fully connected
+                layer
+            target_vocab: the size of the target vocabulary
+            max_seq_len: the maximum sequence length possible
+            drop_rate: the dropout rate
         """
         super(Decoder, self).__init__()
         self.N = N
         self.dm = dm
-        self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=target_vocab, output_dim=dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
         self.blocks = [
-            DecoderBlock(dm, h, hidden, drop_rate) for _ in range(N)
-        ]
+            DecoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
-    def call(self, x, encoder_output, training, look_ahead_mask,
-              padding_mask):
+    def call(
+            self, x, encoder_output, training, look_ahead_mask,
+            padding_mask):
         """
-        x - a tensor of shape (batch, target_seq_len, dm) containing
-            the input to the decoder
-        encoder_output - a tensor of shape (batch, input_seq_len, dm)
-            containing the output of the encoder
-        training - a boolean to determine if the model is training
-        look_ahead_mask - the mask to be applied to the first multi
-            head attention layer
-        padding_mask - the mask to be applied to the second multi head
-            attention layer
+        Args:
+            x: tensor of shape (batch, target_seq_len, dm) containing
+                the input to the decoder
+            encoder_output: tensor of shape (batch, input_seq_len, dm)
+                containing the output of the encoder
+            training: boolean to determine if the model is training
+            look_ahead_mask: the mask to be applied to the first multi
+                head attention layer
+            padding_mask: the mask to be applied to the second multi
+                head attention layer
 
-        Returns: a tensor of shape (batch, target_seq_len, dm)
-            containing the decoder output
+        Returns:
+            a tensor of shape (batch, target_seq_len, dm) containing
+            the decoder output
         """
         seq_len = x.shape[1]
 
@@ -55,8 +59,9 @@ class Decoder(tf.keras.layers.Layer):
 
         x = self.dropout(x, training=training)
 
-        for i in range(self.N):
-            x = self.blocks[i](x, encoder_output, training,
-                                look_ahead_mask, padding_mask)
+        for block in self.blocks:
+            x = block(
+                x, encoder_output, training, look_ahead_mask,
+                padding_mask)
 
         return x
