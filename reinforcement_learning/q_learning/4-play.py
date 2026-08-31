b81dbdaf1da5296b0819d38d26cf84c486d16f60
leyla-h <13678@holbertonstudents.com>
2026-08-31 15:10:06 +0400

    Update 4-play.py

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python3
-"""Defines a function that has a trained agent play an episode of
-FrozenLake.
-"""
+"""Has the trained agent play an episode"""
 import numpy as np
 
 
 def play(env, Q, max_steps=100):
-    """Has the trained agent play an episode
+    """
+    Has the trained agent play an episode
 
-    Args:
-        env: the FrozenLakeEnv instance
-        Q: a numpy.ndarray containing the Q-table
-        max_steps: the maximum number of steps in the episode
+    env: the FrozenLakeEnv instance
+    Q: numpy.ndarray containing the Q-table
+    max_steps: maximum number of steps in the episode
 
-    Returns:
-        total_rewards, rendered_outputs
-            total_rewards: the total rewards for the episode
-            rendered_outputs: a list of rendered outputs representing
-                the board state at each step
+    Returns: total_rewards, rendered_outputs
+        total_rewards: the total rewards for the episode
+        rendered_outputs: a list of rendered outputs representing
+                           the board state at each step
     """
     state, _ = env.reset()
+    rendered_outputs = [env.render()]
     total_rewards = 0
-    rendered_outputs = []
-
-    rendered_outputs.append(env.render())
 
     for step in range(max_steps):
         action = np.argmax(Q[state])
-
-        new_state, reward, terminated, truncated, _ = env.step(action)
-
+        new_state, reward, terminated, truncated, info = env.step(action)
         rendered_outputs.append(env.render())
-
-        state = new_state
         total_rewards += reward
+        state = new_state
 
         if terminated or truncated:
             break
