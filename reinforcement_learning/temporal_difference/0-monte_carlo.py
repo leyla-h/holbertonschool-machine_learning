c95f188a9558e5ea05caf225bcaa086c8deabf2f
leyla-h <13678@holbertonstudents.com>
2026-08-31 15:18:31 +0400

    Update 0-monte_carlo.py

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -7,7 +7,6 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                  alpha=0.1, gamma=0.99):
     """
     Performs the Monte Carlo algorithm
-
     env: environment instance
     V: numpy.ndarray of shape (s,) containing the value estimate
     policy: function that takes in a state and returns the next
@@ -16,26 +15,26 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
     max_steps: maximum number of steps per episode
     alpha: learning rate
     gamma: discount rate
-
     Returns: V, the updated value estimate
     """
     for ep in range(episodes):
         state, _ = env.reset()
         episode = []
-
         for step in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, info = env.step(
                 action)
             episode.append((state, reward))
             state = next_state
-
             if terminated or truncated:
                 break
 
+        episode = np.array(episode, dtype=int)
         G = 0
-        for state, reward in reversed(episode):
+        for t in reversed(range(len(episode))):
+            state, reward = episode[t]
             G = reward + gamma * G
-            V[state] = V[state] + alpha * (G - V[state])
+            if state not in episode[:t, 0]:
+                V[state] = V[state] + alpha * (G - V[state])
 
     return V
